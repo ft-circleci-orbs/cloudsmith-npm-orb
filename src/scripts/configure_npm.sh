28005d66e55a723eebf6d44b53e7e4486ffd1b0d
Hamza Samih <47009614+hamza-samih@users.noreply.github.com>
2024-05-01 15:23:17 +0100

    Refactor download commands (#5)
    * change npm upload script
    
    * fix filename error in dir
    
    * move oidc to orb
    
    * try sudo but this will probably fail...
    
    * change file extension to .tgz instead of .tar.gz
    
    * fix examples and add npm native command and test
    
    * try including orbs block
    
    * remove extra oidc command
    
    * remve extra backticks on context
    
    * include cloudsmith python orb in orb packing
    
    * try adding repo env var to command
    
    * remove dist_dir from npm publish test
    
    * add alpine test for cli upload
    
    * fix docker image name
    
    * remove alpine and apk build stuff.
    
    * change wording in shell script
    
    * change python and cli install to run steps and remove from script
    
    * remove extra python3
    
    * remove ensurepip
    
    * add oidc command correctly
    
    * remove extra configure defaults
    
    * change pip install to python3
    
    * remove ensurepip and revert to pip3
    
    * remove cloudsmith python orb reference
    
    * try moving env vars around
    
    * try adding in command and re-name auth var
    
    * fix echo shellcheck
    
    * fix small errors
    
    * rename create_npmrc to configure_npm and refactor commands
    
    * rename commands, env vars and other changes
    
    * correctly mask auth token in pipeline echo

---
+++ src/scripts/configure_npm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# shellcheck disable=SC2016
+# shellcheck disable=SC2129
+
+set +e
+
+if [ -z "$CLOUDSMITH_NPM_REGISTRY" ]
+then
+  echo "Unable to configure NPM. Env var CLOUDSMITH_NPM_REGISTRY is not defined. Please run the set_env_vars_for_npm command first."
+  exit 1
+fi
+if [ -z "$CLOUDSMITH_NPM_AUTH_CONFIG" ]
+then
+  echo "Unable to configure NPM. Env var CLOUDSMITH_NPM_AUTH_CONFIG is not defined. Please run the set_env_vars_for_npm command first."
+  exit 1
+fi
+
+npm config set registry="$CLOUDSMITH_NPM_REGISTRY"
+npm config set "$CLOUDSMITH_NPM_AUTH_CONFIG"
+
+echo "NPM has been configured to use Cloudsmith registry with $CLOUDSMITH_NPM_REGISTRY."
