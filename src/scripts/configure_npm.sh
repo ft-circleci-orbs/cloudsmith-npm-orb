b8e6cc47361d39dc80d68674667c85e5b23465bf
Hamza Samih <47009614+hamza-samih@users.noreply.github.com>
2024-07-05 12:52:09 +0100

    add check for npm install in configure-npm (#8)
    * add check for npm install in configure-npm
    
    * add some stuff to the readme

--- src/scripts/configure_npm.sh
+++ src/scripts/configure_npm.sh
@@ -5,6 +5,13 @@
 
 set +e
 
+# Check if npm is installed
+if ! command -v npm &> /dev/null
+then
+  echo "npm is not installed. Please install npm before running this script."
+  exit 1
+fi
+
 if [ -z "$CLOUDSMITH_NPM_REGISTRY" ]
 then
   echo "Unable to configure NPM. Env var CLOUDSMITH_NPM_REGISTRY is not defined. Please run the set_env_vars_for_npm command first."
