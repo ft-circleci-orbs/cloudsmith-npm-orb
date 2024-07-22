60ff9d52e1f04daa37ce754420aae58ef265b24e
Hamza Samih <47009614+hamza-samih@users.noreply.github.com>
2024-07-22 11:25:00 +0100

    Create npm heroku command (#9)
    * Create configure_npm_for_heroku script
    
    * Create command
    
    * Add heroku test app to circleci dir
    
    * Add a new test
    
    * fix shellcheck errors
    
    * include heroku test job in workflow
    
    * include heroku orb and add heroku install command
    
    * move test app one dir above
    
    * try relative path
    
    * add working_dir to orb command
    
    * move back to circleci dir and add cd step
    
    * move one one dir to avoid issues...
    
    * add a pwd to see where this script is running
    
    * try setting the working dir at the job level
    
    * try new paths
    
    * try moving back to .circleci dir
    
    * remove change dir
    
    * try cd step once more without the working dir
    
    * add cd correctly
    
    * add a pwd echo to script
    
    * try adding new path vars
    
    * try new way of referencing orb command
    
    * remove bad cd
    
    * add working_dir param for test
    
    * fix shellcheck for cd error
    
    * fix eof error
    
    * add -f flag to force push and overwrite current heroku git
    
    * add working dir to command push
    
    * try a manual cd into the dir
    
    * manually set nodejs buildpack
    
    * add heroku app name to buildpacks set command
    
    * try setting npm_config_prefix
    
    * try to copy all of subdir into root
    
    * get all files
    
    * try root dir
    
    * Revert "try root dir"
    
    This reverts commit ff8082697f7df5224c2a5cce81c42882d9036929.
    
    * try project/
    
    * try moving cp step to pre-heroku cli install
    
    * trying doing build script
    
    * remove build script
    
    * add some test steps
    
    * change steps
    
    * change test to grep inside package.json
    
    * clean up test
    
    * fix example name
    
    * rename example to reflect command
    
    * remove working dir in example.
    
    * try intentionally break the test
    
    * try breaking test
    
    * include orbs in orb.yml
    
    * add heroku and jq install to command
    
    * check if jq is installed and use jq in block
    
    * add jq check for malformed and fix package.json
    
    * change jq validation
    
    * try new jq
    
    * add some echos
    
    * Update src/examples/confgure_npm_for_heroku.yml
    
    Co-authored-by: robertboulton <robert.boulton@ft.com>
    
    * Update src/examples/confgure_npm_for_heroku.yml
    
    Co-authored-by: robertboulton <robert.boulton@ft.com>
    
    * remove extra elif
    
    * remove unneeded heroku/install in example
    
    * change error message
    
    * change else to elif
    
    * intentionally break app_name for testing
    
    * add error check
    
    * now revert broken app name change
    
    * change description on command
    
    * add verbose explanation for existing prebuild script
    
    * add heroku-prebuild for test
    
    * remove double \
    
    * remove test heroku-prebuild
    
    * Update src/scripts/configure_npm_for_heroku.sh
    
    Co-authored-by: robertboulton <robert.boulton@ft.com>
    
    * Update src/examples/confgure_npm_for_heroku.yml
    
    Co-authored-by: robertboulton <robert.boulton@ft.com>
    
    * Update src/examples/confgure_npm_for_heroku.yml
    
    Co-authored-by: Rob Godfrey <rob.godfrey@gmail.com>
    
    ---------
    
    Co-authored-by: robertboulton <robert.boulton@ft.com>
    Co-authored-by: Rob Godfrey <rob.godfrey@gmail.com>

---
+++ src/scripts/configure_npm_for_heroku.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# shellcheck disable=SC2016
+# shellcheck disable=SC2129
+
+set +e
+# Change to working directory (defaults to .)
+cd "$WORKING_DIR" || exit
+
+# Check if npm is installed
+if ! command -v npm &> /dev/null
+then
+  echo "npm is not installed. Please install npm before running this script."
+  exit 1
+fi
+
+# Check if NPM_REGISTRY and NPM_AUTH_CONFIG are set
+if [ -z "$CLOUDSMITH_NPM_REGISTRY" ]
+then
+  echo "Unable to configure NPM. Env var CLOUDSMITH_NPM_REGISTRY is not defined. Please run the set_env_vars_for_npm command first."
+  exit 1
+fi
+if [ -z "$CLOUDSMITH_NPM_AUTH_CONFIG" ]
+then
+  echo "Unable to configure NPM. Env var CLOUDSMITH_NPM_AUTH_CONFIG is not defined. Please run the set_env_vars_for_npm command first."
+  exit 1
+fi
+
+# Check if Heroku CLI is installed
+if ! command -v heroku &> /dev/null
+then
+    echo "Heroku CLI could not be found. Please install it to proceed."
+    exit 1
+fi
+
+# Check if jq is installed
+if ! command -v jq &> /dev/null
+then
+    echo "jq could not be found. Please install it to proceed."
+    exit 1
+fi
+
+# Check if APP_NAME is set
+if [ -z "$APP_NAME" ]
+then
+  echo "Unable to configure NPM. Env var APP_NAME is not defined. Please specify it as a parameter in your circleci job."
+  exit 1
+fi
+
+# Set environment variables via Heroku CLI
+output=$(heroku config:set NPM_REGISTRY="registry=$CLOUDSMITH_NPM_REGISTRY" --app "$APP_NAME" 2>&1)
+heroku config:set NPM_TOKEN="$CLOUDSMITH_NPM_AUTH_CONFIG" --app "$APP_NAME" &>/dev/null # Silencing output as it contains an auth token
+
+# Check for error message in output
+if echo "$output" | grep -q "Error"; then
+    echo "Heroku config set failed with an error. Please check your Heroku app name is a valid Heroku app."
+    exit 1
+fi
+
+# Check if package.json exists
+if [ ! -f package.json ]; then
+    echo "Looking for package.json in $(pwd)"
+    echo "Unable to configure heroku-prebuild scripts. package.json does not exist."
+    exit 1
+fi
+
+# Check if the scripts block exists
+scripts_block_exists=$(grep -o '"scripts":' package.json)
+
+# Check if heroku-prebuild exists
+heroku_prebuild_exists=$(grep -o '"heroku-prebuild":' package.json)
+
+if [ -n "$scripts_block_exists" ] && [ -n "$heroku_prebuild_exists" ]; then
+    echo "heroku-prebuild script already exists. Please add 'npm config set \$NPM_REGISTRY ; npm config set \$NPM_TOKEN' to your existing heroku-prebuild script. Exiting."
+    exit 1
+elif [ -n "$scripts_block_exists" ]; then
+    # Add or create heroku-prebuild to existing scripts block
+    echo "Adding heroku-prebuild to scripts."
+    jq '.scripts["heroku-prebuild"] = "npm config set $NPM_REGISTRY ; npm config set $NPM_TOKEN"' package.json > package.tmp && mv package.tmp package.json
+fi
+
+# Check if the JSON file is valid
+if ! jq empty "package.json" >/dev/null 2>&1; then
+    echo "could not parse json file. File may be malformed. Exiting."
+    exit 1
+fi
